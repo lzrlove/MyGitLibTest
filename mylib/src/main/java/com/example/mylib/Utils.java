8974939c2b7d4876ae5c6f0751c2c98b21e0eebf
lzrlove <lizong_run@126.com>
2019-11-06 11:28:06 +0800

    第一次提交

---
+++ mylib/src/main/java/com/example/mylib/Utils.java
@@ -0,0 +1,11 @@
+package com.example.mylib;
+
+/**
+ * Create by lizongrun
+ * Create Date 2019/11/6 11:22
+ */
+public class Utils {
+    public static String getStr(){
+        return "测试lib库";
+    }
+}
